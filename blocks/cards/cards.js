a743eb1681e7fd35f07975cf207b5ffbb4286e49
Dzianis Mantsevich <dmantsevich@exadel.com>
2026-07-30 17:23:45 +0200

    Add tagline + fonts

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,4 +1,3 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
 import { getContent } from '../../scripts/utils.js';
 
 function createCard(card) {
@@ -9,10 +8,11 @@ function createCard(card) {
     image: { $: 'div:nth-of-type(1) > picture', type: 'html' },
     heading: { $: 'div:nth-of-type(2) > h1' },
     description: { $: 'div:nth-of-type(2) > p', type: 'html' },
+    tagline: { $: 'div:nth-of-type(3) > p', type: 'html' },
   });
   hpeCard.innerHTML = [
     content.image ? `<hpe-image slot="media" src="${content.image.querySelector('img').src}" alt="Abstract blue glass texture" aspect-ratio="16:9"></hpe-image>` : ``,
-    `<span slot="tagline">Tagline label</span>`,
+    content.tagline ? `<span slot="tagline">${content.tagline.innerHTML}</span>` : ``,
     content.heading ? `<span slot="heading">${content.heading}</span>` : ``,
     content.description ? `<hpe-paragraph slot="body" size="md">${content.description.innerHTML}</hpe-paragraph>` : ``,
     `          <hpe-button-group slot="actions" orientation="vertical">
